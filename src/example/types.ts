e0c10e421243ebab164994cf5cd964c951c312ff
CH Jang <tlqckd0@gmail.com>
2022-08-04 23:06:59 +0900

    제거

--- src/example/types.ts
+++
@@ -1,9 +0,0 @@
-const TYPES = {
-    Warrior: Symbol.for("Warrior"),
-    Weapon: Symbol.for("Weapon"),
-    ThrowableWeapon: Symbol.for("ThrowableWeapon"),
-    Point : Symbol.for("Point22"),
-    Circle : Symbol.for("sampleCircle")
-};
-
-export { TYPES };
